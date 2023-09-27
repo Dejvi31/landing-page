5bbf780a839cce6ab81d7af18ca0eebc9c31a231
Dejvi <dejviepadejvi@gmail.com>
2023-09-27 12:16:10 +0200

    krijuam projektin ne vite dhe shtuam navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../styles/Navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav>
+        <span>DE</span>
+        <ul>
+            <li>Home</li>
+            <li>Latest</li>
+            <li>Popular</li>
+            <li>Contact</li>
+            <li>About</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
